6ba7904923341b1c865361e8ec64b5d6181f8055
Gabriel Mateus <gabrielmsouza@outlook.com.br>
2024-03-03 16:57:47 -0300

    feat: initial commit

---
+++ src/test/java/com/github/gabrielmateusms/openfeignexample/OpenfeignExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gabrielmateusms.openfeignexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenfeignExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
